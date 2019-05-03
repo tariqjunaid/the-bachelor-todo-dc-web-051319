ec33eb5886e0bd093af1dafdff63c16808f2aa6a
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-05-03 23:20:48 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant|
-    
+    if contestant[status] == "winner"
+      
 end
 
 def get_contestant_name(data, occupation)
