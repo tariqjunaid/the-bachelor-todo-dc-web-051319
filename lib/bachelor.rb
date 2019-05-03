315f72a17be8036f072508aca8d60b92554a92bd
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-05-03 23:43:18 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -43,5 +43,12 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  contestant_ages = data[season].collect do |contestant|
+    contestant["age"].to_i
+  end
+
+  summed_ages = contestant_ages.reduce(0) {|sum, age| sum += age}
+  contestant_count =  contestant_ages.count.to_f
+  (summed_ages/contestant_count).round
+
 end
