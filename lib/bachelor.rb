979a458ceaff54333c15411b6eb12b889169b98d
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-05-03 23:27:44 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant|
     if contestant["status"] == "Winner"
-      return contestant["name"].split(" ")
+      return contestant["name"].split("")
     end
   end
 end
