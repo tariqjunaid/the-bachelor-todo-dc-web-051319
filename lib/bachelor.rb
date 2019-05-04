a62acd2fe21de7330d3ea027b5ff281f83435937
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-05-04 00:34:29 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -32,7 +32,7 @@ def get_occupation(data, hometown)
   data.each do |hash_season, contestants_array|
     contestants_array.each do |contestant|
       if contestant["hometown"] == hometown
-        contestant["occupation"]
+        return contestant["occupation"]
       end
     end
   end
