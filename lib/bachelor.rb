e34d4636dde6c9d31591c99406b4a927af3a9dce
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-05-04 00:35:32 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -42,7 +42,7 @@ def get_average_age_for_season(data, season)
   age = 0
   count = 0
   data[season].each do |contestant|
-    contestant.each do |k,v|
+    contestant.each do |k|
       if k == "age"
         count += 1
         age += v.to_f
